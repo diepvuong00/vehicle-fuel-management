5a86411cad90c44f1fdd1699944c88558aa4734e
dinhdiepvuong <diepvuong.py@gmail.com>
2026-05-04 10:27:28 +0700

    add cores and port fe

---
+++ src/main/java/deoca/hhv/transport/config/CoresConfig.java
@@ -0,0 +1,19 @@
+package deoca.hhv.transport.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CoresConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // FE
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
